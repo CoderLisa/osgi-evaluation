3bf75858eeb9b2eda5ac05a4eaa21ce966685d08
Lisa Clark <lisa.clark@rackspace.com>
2013-05-13 17:24:02 -0500

    Adding sample bundle

---
+++ knoplerfish/README.md
@@ -0,0 +1,1 @@
+http://www.knopflerfish.org/releases/current/docs/tutorials/kf_osgi_tutorial/kf_osgi_tutorial.pdf
---
+++ knoplerfish/src/main/java/org/osgiexample/knoplerfish/bundle/impl/Activator/HelloWorldActivator.java
@@ -0,0 +1,27 @@
+package org.osgiexample.knoplerfish.bundle.impl.Activator;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class HelloWorldActivator implements BundleActivator {
+
+    public static BundleContext bundleContext = null;
+    private HelloWorldThread thread = null;
+
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        System.out.println("HelloWorldBundle starting...");
+        HelloWorldActivator.bundleContext = bundleContext;
+        thread = new HelloWorldThread();
+        thread.start();
+
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        System.out.println("HelloWorldBundle stopping...");
+        HelloWorldActivator.bundleContext = null;
+        thread.stopThread();
+        thread.join();
+    }
+}
